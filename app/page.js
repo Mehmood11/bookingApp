1a890a5e165dac887087350a89390ac6c8f0f7e9
zakaullah-dev <zaka.ullah@ceative.co.uk>
2023-11-22 16:21:31 +0500

    make generic card

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import styles from './page.module.css'
-import { CustomJobStepperSection } from '@/public/calendar-card/calendar-card'
+import styles from "./page.module.css";
+import { CustomJobStepperSection } from "@/public/calendar-card/calendar-card";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <CustomJobStepperSection />
+      {/* <CustomJobStepperSection /> */}
     </main>
-  )
+  );
 }
