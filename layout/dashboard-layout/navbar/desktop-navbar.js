1b5bcb19357d75f322b09ce19a4de10be1c4484b
Mehmood khan <mehmood.khan@ceative.co.uk>
2023-11-23 11:57:06 +0500

    Header Added

---
+++ layout/dashboard-layout/navbar/desktop-navbar.js
@@ -0,0 +1,90 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Link from "next/link";
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+// google fonts
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import logo from "../../../assets/logo.svg";
+
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const pagesNavbar = [
+  { id: 1, title: "About Us", link: "/about" },
+  { id: 2, title: "Flight Schedule", link: "/flight-schedule" },
+  { id: 3, title: "Account Settings", link: "/account-setting" },
+  { id: 4, title: "Manage Bookings", link: "/manage-bookings" },
+];
+
+const DesktopNavbar = ({ handleOpenAdd, handleCloseAdd, anchorElAdd }) => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, display: { xs: "none", lg: "flex" } }}>
+        <Link href="/">
+          <Image src={logo} />
+        </Link>
+      </Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: { xs: "none", lg: "flex" },
+          alignItems: "center",
+          gap: 3,
+        }}
+      >
+        {pagesNavbar.map((page) => (
+          <StyledNavLink
+            key={page.id}
+            href={page.link}
+            onClick={handleCloseAdd}
+          >
+            {page.title}
+          </StyledNavLink>
+        ))}
+      </Box>
+    </>
+  );
+};
+
+export default DesktopNavbar;
+
+//--------------------------------------------------------
+// Styled  components
+
+const StyledNavLink = styled(Link)(({ theme }) => ({
+  // position: "relative",
+  padding: "25px 15px",
+  color: "white",
+  display: "block",
+  borderBottom: "3px solid transparent",
+  textDecoration: "none",
+  textAlign: "center",
+  // fontFamily: poppins.style.fontFamily,
+
+  "&::before": {
+    content: `""`,
+    opacity: 0,
+    // position: "absolute",
+    width: "100%",
+    height: "5px",
+    bottom: "-3px",
+    left: 0,
+    borderTopLeftRadius: "5rem",
+    borderTopRightRadius: "5rem",
+    backgroundColor: theme.palette.primary.main,
+    transition: "opacity .150s",
+  },
+  "&:hover": {
+    "&::before": {
+      opacity: 1,
+    },
+  },
+}));
---
+++ layout/dashboard-layout/navbar/mobile-navbar.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Link from "next/link";
+import { Drawer, Button, IconButton, Box, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { styled } from "@mui/material/styles";
+import logo from "../../../assets/logo.svg";
+
+// google fonts
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const pagesNavbar = [
+  { id: 1, title: "About Us", link: "/about" },
+  { id: 2, title: "Flight Schedule", link: "/flight-schedule" },
+  { id: 3, title: "Account Settings", link: "/account-setting" },
+  { id: 4, title: "Manage Bookings", link: "/manage-bookings" },
+];
+
+const MobileNavbar = ({
+  toggleDrawerMenu,
+  handleOpenAdd,
+  handleCloseAdd,
+  anchorElAdd,
+  openMenu,
+}) => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, display: { xs: "flex", lg: "none" } }}>
+        <Box sx={{ flexGrow: 0 }}>
+            <Button onClick={toggleDrawerMenu}>
+              <MenuIcon />
+            </Button>
+          <Drawer
+            anchor="left"
+            open={openMenu}
+            onClose={toggleDrawerMenu}
+            sx={{ borderRadius: "10px" }}
+          >
+            <Box sx={{ width: "240px", p: 2, borderRadius: 5 }}>
+              <IconButton
+                sx={{ display: "block", ml: "auto" }}
+                onClick={toggleDrawerMenu}
+              >
+                <CloseIcon />
+              </IconButton>
+              {pagesNavbar.map((item) => (
+                <StyledMenuLink key={item.id} href={item.link}>
+                  <Typography>{item.title}</Typography>
+                </StyledMenuLink>
+              ))}
+            </Box>
+          </Drawer>
+        </Box>
+      </Box>
+
+      <Box sx={{ flexGrow: 1, display: { xs: "flex", lg: "none" } }}>
+        <Link href="/">
+          <Image src={logo} />
+        </Link>
+      </Box>
+    </>
+  );
+};
+
+export default MobileNavbar;
+
+//--------------------------------------------------------
+// Styled  components
+
+const StyledMenuLink = styled(Link)(({ theme }) => ({
+  padding: "15px",
+  color: theme.palette.text.secondary,
+  textDecoration: "none",
+  fontFamily: poppins.style.fontFamily,
+}));
