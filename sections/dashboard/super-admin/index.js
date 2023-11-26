dc4a3cd29f838918e4d4552443ee2e41e2fd6ee4
zaka <zaka.ullah@ceative.co.uk>
2023-11-26 10:55:19 +0500

    remove AddExtra component from dashboard

--- sections/dashboard/super-admin/index.js
+++ sections/dashboard/super-admin/index.js
@@ -1,9 +1,8 @@
 "use client"
 import React, { useState } from "react";
-import { Box, Button, Card, CardContent, TableHead } from "@mui/material";
+import { Box, Card, CardContent } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import DashboardCard from "@/components/dashboard-card";
-import AddExtra from "../stepper/add-extra";
 import { TableData, cardData, columns } from "./super-admin-data";
 import TableHeader from "@/components/custom-table/table-header";
 import CustomTable from "@/components/custom-table";
@@ -12,9 +11,7 @@ const SuperAdmin = () => {
   const [searchParam, setSearchParam] = useState();
   return (
     <Box>
-
-      <AddExtra />
-      {/* <Grid container spacing={2} mb="40px">
+      <Grid container spacing={2} mb="40px">
         {cardData.map((item) => (
           <Grid item xs={12} md={6} lg={3}>
             <DashboardCard
@@ -55,7 +52,7 @@ const SuperAdmin = () => {
         isPagination
       />
       </CardContent>
-      </Card> */}
+      </Card>
 
     </Box>
   );
