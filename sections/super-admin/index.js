ca41c56c2195ed6c394e42ad0fd177110e7515b4
zaka <zaka.ullah@ceative.co.uk>
2023-11-27 22:53:36 +0500

    setting page complete

--- sections/super-admin/index.js
+++ sections/super-admin/index.js
@@ -6,16 +6,12 @@ import DashboardCard from "@/components/dashboard-card";
 import { TableData, cardData, columns } from "./super-admin-data";
 import TableHeader from "@/components/custom-table/table-header";
 import CustomTable from "@/components/custom-table";
-import Setting from "../user-details/setting";
 
 const SuperAdmin = () => {
   const [searchParam, setSearchParam] = useState();
   return (
     <Box>
-
-<Setting />
-
-      {/* <Grid container spacing={2} mb="40px">
+      <Grid container spacing={2} mb="40px">
         {cardData.map((item) => (
           <Grid item xs={12} md={6} lg={3} key={item?.id}>
             <DashboardCard
@@ -56,7 +52,7 @@ const SuperAdmin = () => {
         isPagination
       />
       </CardContent>
-      </Card> */}
+      </Card>
 
     </Box>
   );
