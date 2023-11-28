3ca1ceed154b65003e707e9dd12fce8f4b07213a
Abubakar Saddiq <abubakar.saddiq@ceative.co.uk>
2023-11-28 11:33:00 +0500

    #comment

--- sections/flight-details/select-flight/selectFlight.js
+++ sections/flight-details/select-flight/selectFlight.js
@@ -1,6 +1,7 @@
+import AvailableFlightCard from "@/components/available-flight-card/availableFlightCard";
 import CalendarCard from "@/components/calendar-card";
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const cardData = [
   {
@@ -26,7 +27,7 @@ const cardData = [
   {
     title: "Sunday",
     description: "October 15",
-    value: 81.9,
+    value: 80.52,
   },
   {
     title: "Monday",
@@ -41,10 +42,17 @@ const cardData = [
 ];
 
 const SelectFlight = () => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handleMouseDown = () => {
+    setIsPressed(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsPressed(false);
+  };
   return (
-    <Box
-     
-    >
+    <Box>
       <Box sx={{ color: "#465365", fontSize: "24px", fontWeight: 600 }}>
         Available flights this week Delhi to Mumbai
       </Box>
@@ -52,20 +60,43 @@ const SelectFlight = () => {
         sx={{
           display: "flex",
           alignItems: "center",
-          gap: 2,
           overflow: "scroll",
+          "&::-webkit-scrollbar": {
+            height: "8px",
+          },
+          gap: 2,
           mt: 4,
         }}
       >
         {cardData.map((item) => {
           return (
-            <CalendarCard
-              title={item.title}
-              description={item.description}
-              value={item.value}
-            />
+            <Box sx={{ mb: 0.8 }}>
+              <CalendarCard
+                title={item.title}
+                description={item.description}
+                value={item.value}
+              />
+            </Box>
           );
         })}
+      </Box>
+      <Box></Box>
+
+      <Box>
+        <Box
+          sx={{
+            color: "#465365",
+            fontSize: "24px",
+            fontWeight: 600,
+            marginY: 4,
+          }}
+        >
+          Available Flights from <span style={{ color: "#2278C8" }}>Delhi</span>{" "}
+          to <span style={{ color: "#2278C8" }}>Mumbai</span>
+        </Box>
+        <Box sx={{ height: "50vh", overflowY: "scroll" }}>
+          <AvailableFlightCard />
+        </Box>
       </Box>
     </Box>
   );
